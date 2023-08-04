b2f8733c40b6adad331c01b119a1a68b9c4896e9
MING <lxming95@163.com>
2023-08-04 09:49:56 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -32,7 +32,7 @@ def load_conf():
             "TG_BOT_TOKEN": os.environ.get("TG_BOT_TOKEN", None),
             "TG_USER_ID": os.environ.get("TG_USER_ID", None),
             "TG_BOT_API": os.environ.get("TG_BOT_API", None),
-            "openId": os.environ.get("openId", None),
+            "openId": os.environ.get("openId"),
         }
         conf_kwargs.update({"env_conf": True})
     else:
